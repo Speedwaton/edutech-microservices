3580c8a9e65eff4a9d549226d7a35472e030e5a5
Vicente Castro <vicen.castroa@duocuc.cl>
2025-06-30 15:30:00 -0400

    funcionalidad: agregar repositorios para m├│dulo de cursos

---
+++ ms-courses/src/main/java/com/edutech/courses/repository/EnrollmentRepository.java
@@ -0,0 +1,9 @@
+package com.edutech.courses.repository;
+
+import com.edutech.courses.entity.Enrollment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EnrollmentRepository extends JpaRepository<Enrollment, Integer> {
+}
